d08978ef9c3df91879325f58e1b4149338679eca
“monday-maker” <1512753709@qq.com>
2025-03-18 12:33:45 +0800

    第一次提交

---
+++ 001/001.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout << "Please enter A and B : ";
+    cin >> a >> b;
+    cout << "A + B = "<<a+b << '\n';
+    return 0;
+}
